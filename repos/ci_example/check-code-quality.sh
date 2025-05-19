13408317cbdd54f6a0a9174edb804ceed40c3304
SL2226799 <156187832+SL2226799@users.noreply.github.com>
2025-05-19 11:24:23 +0530

    Delete repos directory

--- repos/ci_example/check-code-quality.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hi"
-black --config .black.toml .
-
-
--- repos/ci_example/example.py
+++
@@ -1,7 +0,0 @@
-"""I'm a python file."""
-
-
-# pylint: disable=invalid-name
-def add_numbers(a: int, b: int) -> int:
-    """Return the sum of two numbers."""
-    return sum([a, b])
